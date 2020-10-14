ebbfb8a226cf990a4a33de80a1c5b03acde5be65
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-10-14 16:14:18 +0200

    Add SharePoint for PowerShell

---
+++ scripts/sharepoint.ps1
@@ -0,0 +1,14 @@
+$config = Get-Content -Path (Join-Path $PSScriptRoot "configuration.json") | ConvertFrom-Json
+[uri]$SiteUri = $config.SiteUrl
+
+$TfmFolder = "netcoreapp3.1"
+if ($PSVersionTable.PSEdition -ine "Core") {
+    $TfmFolder = "net45"
+}
+
+Import-Module -Verbose -ErrorAction "Stop" ([System.IO.Path]::Combine($PSScriptRoot, "..", "publish", "THNETII.SharePoint.PowerShell", "Debug", $TfmFolder, "THNETII.SharePoint.PowerShell.psm1"))
+[THNETII.SharePoint.IdentityModel.SharePointAuthorizationDiscoveryMetadata]`
+$Discovery = Get-SPAuthDiscovery -SiteUri $SiteUri
+
+$Discovery | Format-List *
+
--- src/THNETII.SharePoint.PowerShell/THNETII.SharePoint.PowerShell.psm1
+++ src/THNETII.SharePoint.PowerShell/THNETII.SharePoint.PowerShell.psm1
@@ -1,3 +1,61 @@
-Add-Type -LiteralPath (Join-Path $PSScriptRoot "THNETII.SharePoint.IdentityModel.dll")
 Add-Type -LiteralPath (Join-Path $PSScriptRoot "Microsoft.SharePoint.Client.dll")
 Add-Type -LiteralPath (Join-Path $PSScriptRoot "Microsoft.Identity.Client.dll")
+Add-Type -LiteralPath (Join-Path $PSScriptRoot "Microsoft.IdentityModel.Protocols.dll")
+Add-Type -LiteralPath (Join-Path $PSScriptRoot "THNETII.SharePoint.IdentityModel.dll")
+
+[System.Collections.Generic.IDictionary[string,Microsoft.IdentityModel.Protocols.IConfigurationManager[THNETII.SharePoint.IdentityModel.SharePointAuthorizationDiscoveryMetadata]]] `
+$Script:SPAuthDiscoveryManagers = New-Object "System.Collections.Generic.Dictionary[string,Microsoft.IdentityModel.Protocols.IConfigurationManager[THNETII.SharePoint.IdentityModel.SharePointAuthorizationDiscoveryMetadata]]" `
+    -ArgumentList @([System.StringComparer]::OrdinalIgnoreCase)
+
+function Get-SPAuthDiscoveryManager {
+    [CmdletBinding()]
+    [OutputType([Microsoft.IdentityModel.Protocols.IConfigurationManager[THNETII.SharePoint.IdentityModel.SharePointAuthorizationDiscoveryMetadata]])]
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [ValidateNotNull()]
+        [uri]$SiteUri
+    )
+
+    $ProbeUri = New-Object System.UriBuilder $SiteUri
+    if (-not $ProbeUri.Path.EndsWith("/")) {
+        $ProbeUri.Path += "/"
+    }
+    $ProbeUri.Path += "_vti_bin/client.svc"
+    [string]$ProbeUrl = $ProbeUri.Uri
+    [Microsoft.IdentityModel.Protocols.IConfigurationManager[THNETII.SharePoint.IdentityModel.SharePointAuthorizationDiscoveryMetadata]] `
+    $Manager = $null
+
+    [switch]$Found = $Script:SPAuthDiscoveryManagers.TryGetValue($ProbeUrl, [ref] $Manager)
+    if ($Found) {
+        return $Manager
+    }
+
+    [THNETII.SharePoint.IdentityModel.HttpWwwAuthenticateHeaderParameterRetriever] `
+    $HttpRetriever = New-Object THNETII.SharePoint.IdentityModel.HttpWwwAuthenticateHeaderParameterRetriever
+    [THNETII.SharePoint.IdentityModel.SharePointAuthorizationDiscoveryRetriever] `
+    $MetaRetriever = New-Object THNETII.SharePoint.IdentityModel.SharePointAuthorizationDiscoveryRetriever
+    $Manager = New-Object "Microsoft.IdentityModel.Protocols.ConfigurationManager[THNETII.SharePoint.IdentityModel.SharePointAuthorizationDiscoveryMetadata]" `
+        -ArgumentList @([string]$ProbeUrl,
+            [Microsoft.IdentityModel.Protocols.IConfigurationRetriever[THNETII.SharePoint.IdentityModel.SharePointAuthorizationDiscoveryMetadata]]$MetaRetriever,
+            [Microsoft.IdentityModel.Protocols.IDocumentRetriever]$HttpRetriever)
+    $Script:SPAuthDiscoveryManagers[$ProbeUrl] = $Manager
+    return $Manager
+}
+
+function Get-SPAuthDiscovery {
+    [CmdletBinding()]
+    [OutputType([THNETII.SharePoint.IdentityModel.SharePointAuthorizationDiscoveryMetadata])]
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [ValidateNotNull()]
+        [uri]$SiteUri,
+        [switch]$Force
+    )
+
+    $Manager = Get-SPAuthDiscoveryManager -SiteUri $SiteUri
+    if ($Force) {
+        $Manager.RequestRefresh()
+    }
+    $Manager.GetConfigurationAsync([System.Threading.CancellationToken]::None
+        ).GetAwaiter().GetResult()
+}
